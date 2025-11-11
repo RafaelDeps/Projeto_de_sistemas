302d0970c5a15dba778204c690cc562de6433590
Rafael Deps <108770110+RafaelDeps@users.noreply.github.com>
2025-11-10 22:10:10 -0300

    Add files via upload

---
+++ 05_app_mvc_generics/src/__initi__.py
@@ -0,0 +1,7 @@
+from .controller import *
+from .model import *
+from .repository import *
+from .service import *
+from .util import *
+
+__all__ = ["controller", "model", "repository", "service", "util"]
