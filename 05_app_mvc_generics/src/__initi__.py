5acbcba104c687e72f531c8d4582656fdb2db15f
Rafael Deps <108770110+RafaelDeps@users.noreply.github.com>
2025-11-10 22:38:42 -0300

    Delete 05_app_mvc_generics directory

--- 05_app_mvc_generics/src/__initi__.py
+++
@@ -1,7 +0,0 @@
-from .controller import *
-from .model import *
-from .repository import *
-from .service import *
-from .util import *
-
-__all__ = ["controller", "model", "repository", "service", "util"]
